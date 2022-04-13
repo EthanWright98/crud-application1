d412c322e21b46f38b89b23770840db870207306
Ethan Wright <ethanwright98@hotmail.co.uk>
2022-04-13 11:42:49 +0000

    adding fesitval buttons

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,21 @@
 from application import app
 from flask import render_template
+from application.forms import StageForm, FestivalForm
 
 
 
 @app.route('/')
 def Home():
     return render_template('Home.html')
+
+@app.route('/New_Festival', methods=['GET', 'POST'])
+def New_Festival():
+    form = FestivalForm()
+    return render_template('Festivals.html', form=form)
+
+
+@app.route('/Add_Stage',  methods=['GET', 'POST'] )
+def Add_Stage():
+    form = StageForm()
+    return render_template('Stages.html', form=form)
+
