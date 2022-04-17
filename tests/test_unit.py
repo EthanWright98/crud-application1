4879050c74193daa84742ba96118015e2f58a8e9
Ethan Wright <ethanwright98@hotmail.co.uk>
2022-04-17 22:41:23 +0000

    adding tests

---
+++ tests/test_unit.py
@@ -0,0 +1,32 @@
+from flask import url_for
+from flask_testing import TestCase
+
+from application import app, db
+from application.models import Festivals
+
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(
+            SQLACHEMY_DATABASE_URI="sqlite:///test.db"
+            SECRET_KEY="testsecretkey",
+            WTF_CSRF_ENABLED=False
+        )
+        return app
+    
+    def setup(self):
+        db.create_all()
+        test = Festivals(Name='Test',Start_Date='01/01/2023', End_Date='02/01/2023')
+        db.session.add(test)
+        db.session.commit()
+
+    def teardown(self):
+        db.session.remove()
+        db.drop_all()
+
+class TestView(TestBase)
+    def test_home_get(self):
+        response = self.client.get(url_for('Home'))
+        self.assertEqual(response.status_code, 200)
+
+
