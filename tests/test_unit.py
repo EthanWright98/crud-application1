26d7d14dd357f4c4b918d1511df643e671e4b487
Ethan Wright <ethanwright98@hotmail.co.uk>
2022-04-18 02:04:33 +0000

    adding more tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -9,7 +9,7 @@ from application.models import Festivals
 class TestBase(TestCase):
     def create_app(self):
         app.config.update(
-            SQLACHEMY_DATABASE_URI="sqlite:///test.db"
+            SQLACHEMY_DATABASE_URI="mysql+pymysql://root:letmein1234@10.126.96.3/crudapp1",
             SECRET_KEY="testsecretkey",
             WTF_CSRF_ENABLED=False
         )
@@ -25,9 +25,42 @@ class TestBase(TestCase):
         db.session.remove()
         db.drop_all()
 
-class TestView(TestBase)
+class TestView(TestBase):
     def test_home_get(self):
-        response = self.client.get(url_for('Home'))
+        response = self.client.get(url_for('New_Festival'))
         self.assertEqual(response.status_code, 200)
+
+    def setup(self):
+        db.create_all()
+        test = Stages(Name='Stage1', Festival_id='1')
+        db.session.add(test)
+        db.session.commit()
+
+    def teardown(self):
+        db.session.remove()
+        db.drop_all()
+
+class TestVeiw(TestBase):
+    def test_home_get(self):
+        response = self.client.get(url_for('Add_Stage'))
+        self.assertEqual(response.status_code, 200)
+
+#class TestUpdate(TestBase):
+    #def test_home_edit(self):
+        #response = self.client.post(
+            #url_for('update/<int:id>')
+            #data = dict(Name='Test2',Start_Date='11/01/2023', End_Date='22/01/2023')
+        #),
+        #follow_redirects= True
+        
+        #self assertIn(Name='Test3',Start_Date='22/12/2023', End_Date='25/11/2023', response.data)
+
+
+
+
+
+
+
+
 
 
